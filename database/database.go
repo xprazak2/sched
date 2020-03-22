199b3805fbe05337e571d9a992115359dfb9d8a6
Ondrej Prazak <oprazak@redhat.com>
2020-03-22 12:09:04 +0100

    Parse command line args

--- database/database.go
+++ database/database.go
@@ -8,8 +8,15 @@ import (
 
 var connection *gorm.DB
 
-func Connect() *gorm.DB {
-	conn, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=sched password=postgres sslmode=disable")
+var defaultConnection = "host=127.0.0.1 port=5432 user=postgres dbname=sched password=postgres sslmode=disable"
+
+func Connect(connString string) *gorm.DB {
+	if (connString == "") {
+		fmt.Println("No connection string specified, using the default local connection.")
+		connString = defaultConnection
+	}
+
+	conn, err := gorm.Open("postgres", connString)
 	if err != nil {
 		fmt.Println("db err: ", err)
 		panic("Could not connect to db")
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"github.com/xprazak2/sched/users"
@@ -11,27 +12,17 @@ func migrate(db *gorm.DB) {
 	db.AutoMigrate(&users.UserModel{})
 }
 
-// func initDb() {
-// 	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=gorm password=postgres")
-// 	if err != nil {
-// 		fmt.Println("db err: ", err)
-// 	}
-// 	return db
-// }
 
 func main() {
 
-	db := database.Connect()
+	connString := flag.String("db", "", "A connection string to postgres")
+	flag.Parse()
+
+	db := database.Connect(*connString)
 	migrate(db)
   defer db.Close()
 
 	router := gin.Default()
-
-	// router.GET("/ping", func(ctx *gin.Context) {
-	// 	ctx.JSON(200, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
 
 	userRoutes := router.Group("/users")
 	userRoutes.GET("/", users.UsersHandler)
